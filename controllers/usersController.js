426292d5eadc174383bbad478c38c45193d8a72b
RhexNiebres <rhexniebres@gmail.com>
2025-02-27 13:12:00 -0800

    feat: add user creation and update  with email, age, and bio and validation method for email, bio and age field

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,8 +14,8 @@ exports.usersCreateGet = (req, res) => {
 };
 
 exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
+  const { firstName, lastName, email, age, bio } = req.body;
+  usersStorage.addUser({ firstName, lastName, email, age, bio });
   res.redirect("/");
 };
 
@@ -26,12 +26,40 @@ const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
 
 const validateUser = [
-  body("firstName").trim()
-    .isAlpha().withMessage(`First name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
-  body("lastName").trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Email must be in a valid Format sample: spongebob@mail.com")
+    .notEmpty()
+    .withMessage("Email is required"),
+
+  body("age")
+    .isInt({ min: 18, max: 120 })
+    .withMessage("Age must be a number between 18 and 120")
+    .notEmpty()
+    .withMessage("Age is required"),
+
+  body("bio")
+    .trim()
+    .matches(/^[a-zA-Z0-9.,!? ]+$/)
+    .withMessage(
+      "Bio can only contain letters, numbers, spaces and punctuation"
+    )
+    .isLength({ min: 1, max: 200 })
+    .withMessage("bio must be between 1 and 200 characters"),
 ];
 
 // We can pass an entire array of middleware validations to our controller.
@@ -45,41 +73,46 @@ exports.usersCreatePost = [
         errors: errors.array(),
       });
     }
-    const { firstName, lastName } = req.body;
-    usersStorage.addUser({ firstName, lastName });
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
     res.redirect("/");
-  }
+  },
 ];
 
 exports.usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
+};
+
+exports.usersUpdatePost = [
+  validateUser,
+  (req, res) => {
     const user = usersStorage.getUser(req.params.id);
-    res.render("updateUser", {
-      title: "Update user",
-      user: user,
-    });
-  };
-  
-  exports.usersUpdatePost = [
-    validateUser,
-    (req, res) => {
-      const user = usersStorage.getUser(req.params.id);
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).render("updateUser", {
-          title: "Update user",
-          user: user,
-          errors: errors.array(),
-        });
-      }
-      const { firstName, lastName } = req.body;
-      usersStorage.updateUser(req.params.id, { firstName, lastName });
-      res.redirect("/");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user: user,
+        errors: errors.array(),
+      });
     }
-  ];
-  
-  // Tell the server to delete a matching user, if any. Otherwise, respond with an error.
-exports.usersDeletePost = (req, res) => {
-    usersStorage.deleteUser(req.params.id);
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.updateUser(req.params.id, {
+      firstName,
+      lastName,
+      email,
+      age,
+      bio,
+    });
     res.redirect("/");
-  };
-  
+  },
+];
+
+// Tell the server to delete a matching user, if any. Otherwise, respond with an error.
+exports.usersDeletePost = (req, res) => {
+  usersStorage.deleteUser(req.params.id);
+  res.redirect("/");
+};
